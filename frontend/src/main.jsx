983b3b29704deea3ccf851a66f5b80992677f5b0
amy <amy@amypthompson.com>
2025-09-29 01:05:00 +0100

    small edit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 {/*import { StrictMode } from 'react'*/}
 import { createRoot } from 'react-dom/client';
 import { useState } from "react";
-{/*import './index.css'*/}
+import './index.css'
 {/*import App from './App.jsx'*/}
 
 function Typing_design() {
